d876144b0c0f6b47f2b4d7ab086b279df8e0d1ad
VishalDesai <desaidesai62230@gmail.com>
2024-06-22 18:10:31 +0530

    Updated Readme file

--- Beginner/Address-Book/src/components/AllContacts.jsx
+++ Beginner/Address-Book/src/components/AllContacts.jsx
@@ -1,38 +1,54 @@
-import { FaRegEdit, FaHeart } from "react-icons/fa"
+import { FaRegEdit, FaHeart } from "react-icons/fa";
 import { CiHeart } from "react-icons/ci";
 import { MdDelete } from "react-icons/md";
-import userImg from '../assets/userImg.jpg'
-import PropTypes from 'prop-types'
+import userImg from "../assets/userImg.jpg";
+import PropTypes from "prop-types";
 
-
-const AllContacts = ({ contact, handleEdit, handleFav, handleDelete, index }) => {
+const AllContacts = ({
+  contact,
+  handleEdit,
+  handleFav,
+  handleDelete,
+  index,
+}) => {
   const renderImage = () => {
-    if (contact.img && Object.keys(contact.img).length !== 0) {
-      return <img src={URL.createObjectURL(contact.img)} alt="Profile Picture" />;
+    if (contact.img) {
+      const imgSrc =
+        typeof contact.img === "string"
+          ? contact.img
+          : URL.createObjectURL(contact.img);
+      return <img src={imgSrc} alt="Profile Picture" />;
     } else {
       return <img src={userImg} alt="Default Profile Picture" />;
     }
   };
 
   return (
-    <div className='cards'>
+    <div className="cards">
       {renderImage()}
-      <div className='info'>
-        <p className='account-name'>
+      <div className="info">
+        <p className="account-name">
           <b>{contact.name}</b>
           <span>{contact.des}</span>
         </p>
-        <p className='account-contact'>
+        <p className="account-contact">
           <strong>{contact.num}</strong>
           <span>{contact.email}</span>
         </p>
       </div>
-      <div className='account-button'>
-        <button className='edit-btn' onClick={() => handleEdit(index)}><FaRegEdit/></button>
-        <button className={`fav ${contact.fav ? 'clicked' : ''}`} onClick={() => handleFav(index)}>
-          {contact.fav ? <FaHeart/> : <CiHeart/>}
+      <div className="account-button">
+        <button className="edit-btn" onClick={() => handleEdit(index)}>
+          <FaRegEdit />
         </button>
-        <button className='delete' onClick={() => handleDelete(index)}><MdDelete/></button>
+        <button
+          className={`fav ${contact.fav ? "clicked" : ""}`}
+          onClick={() => handleFav(index)}
+        >
+          {contact.fav ? <FaHeart /> : <CiHeart />}
+        </button>
+        <button className="delete" onClick={() => handleDelete(index)}>
+          <MdDelete />
+        </button>
       </div>
     </div>
   );
@@ -43,7 +59,7 @@ AllContacts.propTypes = {
   handleEdit: PropTypes.func,
   handleFav: PropTypes.func,
   handleDelete: PropTypes.func,
-  index: PropTypes.number
+  index: PropTypes.number,
 };
 
 export default AllContacts;
